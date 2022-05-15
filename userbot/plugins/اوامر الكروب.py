c5d958c33bc10498210a307719544ce737b7d104
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-05-15 02:15:41 -0700

    Update اوامر الكروب.py

--- userbot/plugins/اوامر الكروب.py
+++ userbot/plugins/اوامر الكروب.py
@@ -484,6 +484,34 @@ async def event(vois):
     if jpvois54:
         await vois.client.send_file(vois.chat_id, jpvois54, reply_to=Ti)
         await vois.delete()        
+@jmthon.on(events.NewMessage(outgoing=True, pattern="^.ارسل ?(.*)"))
+
+async def remoteaccess(event):
+
+    p = event.pattern_match.group(1)
+    m = p.split(" ")
+
+    chat_id = m[0]
+    try:
+        chat_id = int(chat_id)
+    except BaseException:
+
+        pass
+
+    msg = ""
+    mssg = await event.get_reply_message()
+    if event.reply_to_msg_id:
+        await event.client.send_message(chat_id, mssg)
+        await event.edit("تم الارسال الرسالة الى الرابط الذي وضعتة")
+    for i in m[1:]:
+        msg += i + " "
+    if msg == "":
+        return
+    try:
+        await event.client.send_message(chat_id, msg)
+        await event.edit("تم ارسال الرساله الى الرابط الذي وضعتة")
+    except BaseException:
+        await event.edit("** عذرا هذا ليست مجموعة **")
 @jmthon.ar_cmd(
     pattern="اطردني$",
     command=("اطردني", plugin_category),
