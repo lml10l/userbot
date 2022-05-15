341aee2daed01e8393245d41c298d6e350022275
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-05-15 12:42:20 +0300

    Update فحص.py

--- userbot/plugins/فحص.py
+++ userbot/plugins/فحص.py
@@ -37,7 +37,7 @@ async def amireallyalive(event):
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "✇ ◅"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**[𐇮 𝙅 َِ𝙀 َِ𝙋 َِ𝙏 َِ𝙃 َِ𝙊 َِ𝙉  ](t.me/Jepthon)**"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**[ 𝗝𝗘𝗣𝗧𝗛𝗢𝗡 𝗜𝗦 𝗪𝗢𝗥𝗞𝗜𝗡𝗚 ✓ ](t.me/Jepthon)**"
     RR7_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC or "https://telegra.ph/file/fc5f9ec39e106e130cf2e.jpg"
     jmthon_caption = gvarstatus("ALIVE_TEMPLATE") or temp
     caption = jmthon_caption.format(
